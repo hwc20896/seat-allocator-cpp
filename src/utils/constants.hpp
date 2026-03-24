93199d59c0c990901d41145765d44bfcc5471e26
hwc20896 <hwc20896@gmail.com>
2026-03-25 07:29:33 +0800

    feat(app): add shuffle animation sound effect
    - Include sound resource file in CMake build system
    - Add constants header with default color and sound path
    - Create resource file for effects sounds
    - Register sound file under /effects prefix in Qt resource system

---
+++ src/utils/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Constants {
+    inline constexpr auto DEFAULT_COLOR = "black";
+
+    inline constexpr auto SHUFFLE_ANIMATION_SOUND = "qrc:/effects/sounds/snd_pling.wav";
+}
